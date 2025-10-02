b93c771e7eb08c7b91e1590c26b2b4e0f44c2fbc
sim-berg <sim-berg@users.noreply.github.com>
2025-10-02 10:00:19 +0000

    deploy: 8ca10b418847b07cd4d26fe0da29ad47bd9648ce

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4049375478"
+    serviceWorkerVersion: "3094087510"
   }
 });
